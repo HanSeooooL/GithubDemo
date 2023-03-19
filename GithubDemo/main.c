836fc028009384c6556cf32641fab24e2300ee62
설 한 <5fz6hc6psp@privaterelay.appleid.com>
2023-03-19 22:19:58 +0900

    Initial Commit

---
+++ GithubDemo/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  GithubDemo
+//
+//  Created by 한설 on 2023/03/19.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
